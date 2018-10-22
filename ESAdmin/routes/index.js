c131d36c40032928efb41766ed356d880ab0f08e
Jeremy Yelle <jeremy.yelle@gmail.com>
2018-10-22 17:02:52 -0400

    Overall calculations depend on role specialty now, taking into account data/master nodes.

--- ESAdmin/routes/index.js
+++ ESAdmin/routes/index.js
@@ -2,47 +2,60 @@
 var express = require('express');
 var router = express.Router();
 var sr = require('sync-request');
-
-/*  -- I don't know why I'm leaving this in here, probably because I don't have source control yet and I want to remember how to do async requests. --
- 
-var clusterHealth = "This didn't work";
-var nodeStatus = "This didn't work";
-var clusterStats = "This didn't work";
-var clusterResources = { cpu: 0, mem: 0, disk: 0 }; 
-
-
-request('http://192.168.56.50:9200/_cluster/health', function (err, resp, body) {
-    clusterHealth = JSON.parse(body);    
-});
-
-request('http://192.168.56.50:9200/_nodes', function (err, resp, body) {
-    nodeStatus = JSON.parse(body);
-});
-
-request('http://192.168.56.50:9200/_cluster/stats', function (err, resp, body) {
-    clusterStats = JSON.parse(body);
-    clusterResources.cpu = clusterStats.nodes.process.cpu.percent;
-    clusterResources.mem = clusterStats.nodes.os.mem.used_percent;
-    clusterResources.disk = parseInt((clusterStats.nodes.fs.total_in_bytes - clusterStats.nodes.fs.free_in_bytes) / clusterStats.nodes.fs.total_in_bytes * 100);
-});  */
-
-
-
+var traverse = require('traverse');
 
 /* GET home page. */
 router.get('/', function (req, res) {
     var clusterHealth = "This didn't work";
-    var nodeStatus = "This didn't work";
-    var clusterStats = "This didn't work";
+    var nodeStatus = {};
+    //var clusterStats = "This didn't work";
+    var nodeStats = "This didn't work";
+    var memSum = 0;
+    var memCount = 0;
+    var diskSum = 0;
+    var diskCount = 0;
+    var cpuSum = 0;
+    var cpuCount = 0;
+    var nodeCount = 0;
     var clusterResources = { cpu: 0, mem: 0, disk: 0 };
   
     clusterHealth = JSON.parse(sr('GET', 'http://192.168.56.50:9200/_cluster/health').getBody('utf8')); 
-    nodeStatus = JSON.parse(sr('GET', 'http://192.168.56.50:9200/_nodes').getBody('utf8'));
-    clusterStats = JSON.parse(sr('GET', 'http://192.168.56.50:9200/_cluster/stats').getBody('utf8'));
+    //nodeStatus = JSON.parse(sr('GET', 'http://192.168.56.50:9200/_nodes').getBody('utf8'));
+    //clusterStats = JSON.parse(sr('GET', 'http://192.168.56.50:9200/_cluster/stats').getBody('utf8'));
+    nodeStats = JSON.parse(sr('GET', 'http://192.168.56.50:9200/_nodes/stats').getBody('utf8'));
 
-    clusterResources.cpu = clusterStats.nodes.process.cpu.percent;
-    clusterResources.mem = parseInt(clusterStats.nodes.jvm.mem.heap_used_in_bytes / clusterStats.nodes.jvm.mem.heap_max_in_bytes * 100);
-    clusterResources.disk = parseInt((clusterStats.nodes.fs.total_in_bytes - clusterStats.nodes.fs.free_in_bytes) / clusterStats.nodes.fs.total_in_bytes * 100);
+    traverse(nodeStats.nodes).forEach(function (item) {
+        if (item.hasOwnProperty('roles')) {
+            var dataNode = false;
+            var nodeRoles = traverse(this.node.roles).reduce(function (acc, x) {
+                if (this.isLeaf) acc.push(x);
+                return acc;
+            }, []);
+
+            nodeRoles.forEach(function (element) {
+                if (element === 'data') {
+                    dataNode = true;
+                }
+            });            
+
+            if (dataNode) {
+                memSum += this.node.jvm.mem.heap_used_percent;
+                memCount += 1;
+                diskSum += parseInt((this.node.fs.total.total_in_bytes - this.node.fs.total.free_in_bytes) / this.node.fs.total.total_in_bytes );
+                diskCount += 1;
+            }
+
+            cpuSum += this.node.os.cpu.percent;
+            cpuCount += 1;
+
+            nodeStatus[nodeCount] = { name: this.node.name, host: this.node.host, roles: nodeRoles };
+            nodeCount++;
+        }
+    });
+
+    clusterResources.cpu = parseInt(cpuSum / cpuCount);
+    clusterResources.mem = parseInt(memSum / memCount);
+    clusterResources.disk = parseInt(diskSum / diskCount);
 
     res.render('index', { title: 'ES Monitor Overview', clusterHealth: clusterHealth, nodeStatus: nodeStatus, clusterResources: clusterResources });
 });
